a3a6a5b6e9de5f1938d6e32c815436285b31ea96
Santiago <santiago.calderon@correounivalle.edu.co>
2021-03-15 20:35:30 -0500

    Varias Pruebas con Enzyme

--- src/contador.js
+++ src/contador.js
@@ -1,26 +1,25 @@
-import react, { useState } from 'react'
-import PropTypes from 'prop-types'
+import react, { useState } from "react";
+import PropTypes from "prop-types";
 
-const Contador = ({propNum}) => {
-    const [cuenta, setCuenta] = useState(propNum);
+const Contador = ({ propNum }) => {
+  const [cuenta, setCuenta] = useState(propNum);
 
-    return (
-        <>
-        <p>HOLA SOY CONTADOR Fredy</p>
-        <h1>HOLA SOY FREDY</h1>
-        <h2>{cuenta}</h2>
-        <button onClick={( () => setCuenta(cuenta + 1))} > ¡Más!</button>
-        <button onClick={( () => setCuenta(cuenta - 1))} > ¡Menos!</button>
-        <br/>
-        <br/>
-        <button onClick={( () => setCuenta(propNum))} > ¡Reset!</button>
-       
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Hola Soy Contador Santiago</h1>
+      <h2>Hola soy Santiago</h2>
+      <h3>{cuenta}</h3>
+      <button onClick={() => setCuenta(cuenta + 1)}> ¡Más!</button>
+      <button onClick={() => setCuenta(cuenta - 1)}> ¡Menos!</button>
+      <br />
+      <br />
+      <button onClick={() => setCuenta(propNum)}> ¡Reset!</button>
+    </>
+  );
+};
 
 Contador.propTypes = {
-    propNum: PropTypes.number.isRequired
-}
+  propNum: PropTypes.number.isRequired,
+};
 
 export default Contador;
--- src/tests/contador.test.js
+++ src/tests/contador.test.js
@@ -1,55 +1,79 @@
-import Contador from '../contador'
+import Contador from "../contador";
 //import { render } from '@testing-library/react'
 //import '@testing-library/jest-dom/extend-expect'
-import { shallow } from 'enzyme'
+import { shallow } from "enzyme";
 
+describe("Pruebas sobre el componente contador", () => {
+  test("Buscar parrafo", () => {
+    // const { getByText } = render(<Contador propNum={0}/>)
+    //  expect( getByText('HOLA SOY CONTADOR')).toBeInTheDocument()
+  });
 
+  test("Probando con enzyme", () => {
+    const wrapper = shallow(<Contador propNum={0} />);
+    expect(wrapper).toMatchSnapshot();
+  });
 
-describe('Pruebas sobre el componente contador',()=>{
+  test("Busando parrafo en el componente", () => {
+    const wrapper = shallow(<Contador propNum={0} />);
+    const textoparrado = wrapper.find("h1").text();
+    const textoenparra = "Hola Soy Contador Santiago";
 
-   test('Buscar parrafo',()=>{
-  // const { getByText } = render(<Contador propNum={0}/>)
-  //  expect( getByText('HOLA SOY CONTADOR')).toBeInTheDocument()
+    expect(textoenparra).toBe(textoparrado);
 
-   })
-
-
-   test('Probando con enzyme', ()=>{
-
-    const wrapper = shallow(<Contador propNum={0} />)
-    expect(wrapper).toMatchSnapshot()
-
-})
-
-
-test('Busando parrafo en el componente', ()=>{
-
-    const wrapper = shallow(<Contador propNum={0} />)
-    const textoparrado = wrapper.find('p').text()
-    const textoenparra = 'HOLA SOY CONTADOR Fredy'
-
-    expect(textoenparra).toBe(textoparrado)
-    
     //expect(wrapper).toMatchSnapshot()
+  });
 
-})
+  test("Traer button", () => {
+    const wrapper = shallow(<Contador propNum={23} />);
+    const btn1 = wrapper.find("button").at(0);
+    console.log(btn1.html());
+  });
 
-test('Traer button',() => {
-    const wrapper = shallow(<Contador propNum={10} />)
-    const btn1 = wrapper.find('button').at(0)
-    console.log(btn1.html())
-  })
+  test("Simular Click Boton Suma", () => {
+    const wrapper = shallow(<Contador propNum={23} />);
+    const btn1 = wrapper.find("button").at(0).simulate("click");
+    const contando = wrapper.find("h3").text().trim();
+    expect(contando).toBe("24");
+  });
 
+  test("Simular Click Boton Resta", () => {
+    const wrapper = shallow(<Contador propNum={23} />);
+    const btn1 = wrapper.find("button").at(1).simulate("click");
+    const contando = wrapper.find("h3").text().trim();
+    expect(contando).toBe("22");
+  });
 
-  test('Simular click boton suma',() => {
-    const wrapper = shallow(<Contador propNum={10} />)
-    const btn1 = wrapper.find('button').at(0).simulate('click')
-    const contando = wrapper.find('h2').text().trim()
-    expect(contando).toBe('11')
-  
-  })
-  
-  
+  test("Simular Click Boton Reset", () => {
+    const wrapper = shallow(<Contador propNum={23} />);
+    const btn1 = wrapper.find("button").at(2).simulate("click");
+    const contando = wrapper.find("h3").text().trim();
+    expect(contando).toBe("23");
+  });
 
+  test("Simular Varios Clicks", () => {
+    const wrapper = shallow(<Contador propNum={23} />);
+    const btn1 = wrapper.find("button").at(0).simulate("click");
+    btn1.simulate("click");
+    btn1.simulate("click");
+    btn1.simulate("click");
+    const btn2 = wrapper.find("button").at(1).simulate("click");
+    btn2.simulate("click");
+    btn2.simulate("click");
+    btn2.simulate("click");
+    const contando = wrapper.find("h3").text().trim();
+    expect(contando).toBe("23");
+  });
 
-})
+  test("Encontrar 2 br", () => {
+    const wrapper = shallow(<Contador propNum={23} />);
+
+    let bool = true;
+    for (let i = 0; i < 2; i++) {
+      if (wrapper.find("br").at(i).html() !== "<br/>") {
+        bool = false;
+      }
+    }
+    expect(bool).toBe(true);
+  });
+});
